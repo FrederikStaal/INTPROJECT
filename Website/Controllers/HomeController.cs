8c780340c7afd3fca7750b2f31e2a309bc417779
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2020-05-24 13:33:14 +0200

    Update HomeController.cs

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 using Microsoft.Extensions.Logging;
 
 namespace Website.Controllers
@@ -12,7 +13,28 @@ namespace Website.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // Cookies
+
+            //check for 'first_request' on client
+            if (!HttpContext.Request.Cookies.ContainsKey("first_request"))
+            {
+                // Insert whatever data we want in a cookie on the client
+                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString());
+                //return Content("Welcome, new visitor!");
+                return View("~/Views/Game/Index.cshtml");
+            } else
+            {
+                //read client's cookie
+                DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]);
+                //return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString());
+                return View("~/Views/Game/Index.cshtml");
+            }
+            
+            
+
+
+            //return game view
+            //return View("~/Views/Game/Index.cshtml");
         }
     }
 }
