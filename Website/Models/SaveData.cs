f9f63fa9eee375f24adfe5f8f1e68415d1541eb8
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2020-05-25 10:21:21 +0200

    Create SaveData.cs

---
+++ Website/Models/SaveData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Models
+{
+    public class SaveData
+    {
+        int Turn;
+        int Cid;
+        int Mil;
+        int Hap;
+        int Eco;
+        int Rel;
+
+        public SaveData(int turn, int cid, int mil, int hap, int eco, int rel)
+        {
+            Turn = turn;
+            Cid = cid;
+            Mil = mil;
+            Hap = hap;
+            Eco = eco;
+            Rel = rel;
+        }
+    }
+}
