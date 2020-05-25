d0b485e7a14b51641da3fd57183f840f6be39b98
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2020-05-25 10:21:19 +0200

    Update GameController.cs

--- Website/Controllers/GameController.cs
+++ Website/Controllers/GameController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Website.Models;
 
 namespace Website.Controllers
 {
@@ -10,28 +13,49 @@ namespace Website.Controllers
     {
         public IActionResult Index()
         {
-            //return View();
-            
-            return View("~/Views/Game/Index.cshtml");
+            if (!HttpContext.Request.Cookies.ContainsKey("savedata"))
+            {
+                return View("~/Views/Game/Index.cshtml");
+            } else
+            {
+                return View("~/Views/Game/Index.cshtml");
+            }
+            //return View("~/Views/Game/Index.cshtml");
 
         }
 
-        public IActionResult Cookie()
+        public void SaveData(int turn, int cid, int mil, int hap, int eco, int rel)
         {
-            if (!HttpContext.Request.Cookies.ContainsKey("first_request"))
-            {
-                // Insert whatever data we want in a cookie on the client
-                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString());
-                //return Content("Welcome, new visitor!");
-                return View("~/Views/Game/Index.cshtml");
-            }
-            else
-            {
-                //read client's cookie
-                DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]);
-                //return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString());
-                return View("~/Views/Game/Index.cshtml");
-            }
+            SaveData sd = new SaveData(1, 2, 3, 4, 5, 6);
+            HttpContext.Response.Cookies.Append("savedata", JsonConvert.SerializeObject(sd));
         }
+
+
+        public string GetData()
+        {
+            //return JsonConvert.SerializeObject(new SaveData(turn, cid, mil, hap, eco, rel));
+            //new SaveData(turn, cid, mil, hap, eco, rel);
+            return HttpContext.Request.Cookies["savedata"];
+        }
+
+
+       
+        //public IActionResult Cookie()
+        //{
+        //    if (!HttpContext.Request.Cookies.ContainsKey("first_request"))
+        //    {
+        //        // Insert whatever data we want in a cookie on the client
+        //        HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString());
+        //        //return Content("Welcome, new visitor!");
+        //        return View("~/Views/Game/Index.cshtml");
+        //    }
+        //    else
+        //    {
+        //        //read client's cookie
+        //        DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]);
+        //        //return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString());
+        //        return View("~/Views/Game/Index.cshtml");
+        //    }
+        //}
     }
 }
