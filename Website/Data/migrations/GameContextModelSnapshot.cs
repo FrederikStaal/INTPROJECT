3a34926181f47e6ea4e5487fdb5f36175be0b6cd
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2020-05-26 13:44:55 +0200

    Update GameContextModelSnapshot.cs

--- Website/Data/migrations/GameContextModelSnapshot.cs
+++ Website/Data/migrations/GameContextModelSnapshot.cs
@@ -62,6 +62,51 @@ namespace Website.Data.Migrations
 
                     b.ToTable("Card");
                 });
+
+            modelBuilder.Entity("Website.Models.SituationCard", b =>
+                {
+                    b.Property<int>("SituationCardID")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<int>("CharacterID")
+                        .HasColumnType("int");
+
+                    b.Property<int>("Economy1")
+                        .HasColumnType("int");
+
+                    b.Property<int>("Economy2")
+                        .HasColumnType("int");
+
+                    b.Property<int>("Happiness1")
+                        .HasColumnType("int");
+
+                    b.Property<int>("Happiness2")
+                        .HasColumnType("int");
+
+                    b.Property<string>("ImageRef")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("Military1")
+                        .HasColumnType("int");
+
+                    b.Property<int>("Military2")
+                        .HasColumnType("int");
+
+                    b.Property<int>("Relations1")
+                        .HasColumnType("int");
+
+                    b.Property<int>("Relations2")
+                        .HasColumnType("int");
+
+                    b.Property<string>("Text")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("SituationCardID");
+
+                    b.ToTable("SituationCard");
+                });
 #pragma warning restore 612, 618
         }
     }
