5db50e761772e85c96baf22b681f10b77583a11c
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2020-05-26 12:26:49 +0200

    Update GameContextModelSnapshot.cs

--- Website/Data/migrations/GameContextModelSnapshot.cs
+++ Website/Data/migrations/GameContextModelSnapshot.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Website.Models;
 
-namespace Website.Data.migrations
+namespace Website.Data.Migrations
 {
     [DbContext(typeof(GameContext))]
     partial class GameContextModelSnapshot : ModelSnapshot
