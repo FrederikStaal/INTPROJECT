f7c0cc4a926a78164674932070ea9379a02b8151
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2020-05-26 12:26:38 +0200

    Create 20200526102313_InitialCreate.cs

---
+++ Website/Data/Migrations/20200526102313_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Website.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Card",
+                columns: table => new
+                {
+                    CardID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageRef = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true),
+                    Military1 = table.Column<int>(nullable: false),
+                    Military2 = table.Column<int>(nullable: false),
+                    Happiness1 = table.Column<int>(nullable: false),
+                    Happiness2 = table.Column<int>(nullable: false),
+                    Economy1 = table.Column<int>(nullable: false),
+                    Economy2 = table.Column<int>(nullable: false),
+                    Relations1 = table.Column<int>(nullable: false),
+                    Relations2 = table.Column<int>(nullable: false),
+                    CharacterID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Card", x => x.CardID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Card");
+        }
+    }
+}
