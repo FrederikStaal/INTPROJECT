358d040204e148dc3433eae5493495d4a23f7daf
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2020-05-27 08:41:09 +0200

    Update CardsController.cs

--- Website/Controllers/CardsController.cs
+++ Website/Controllers/CardsController.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿/*
+ * Group 6
+ * Rasmus, Joseph, Tony and Frederik
+ * Class type: ControllerBase
+ * - ControllerBase class for the Card API
+ */
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Website.Models;
-using Microsoft.AspNetCore.Cors;
 
 namespace Website.Controllers
 {
